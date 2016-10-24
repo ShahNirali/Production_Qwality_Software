e779dbfec986fa0a15b0db230c556e8b6e100dcc
ShahNirali <nns271@nyu.edu>
2016-10-24 09:06:12 -0400

    PS2 - Added code for equals and hashCode

--- PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchDriver.java
+++ PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchDriver.java
@@ -7,12 +7,37 @@ import edu.nyu.pqs.stopwatch.api.State;
 
 public class StopwatchDriver implements Stopwatch {
   private final String id;
-  private State state;
+  private State state = State.BLOCK;
   private List<Long> lap = new ArrayList<Long>();
   
   public StopwatchDriver (String id) {
     this.id = id;
-    this.state = State.BLOCK;
+  }
+  
+  /*
+   * Synchronizing object of StopwatchDriver to add lap times
+   */
+  private synchronized void addLap() {
+    if (lap.isEmpty()) {
+      lap.add(System.currentTimeMillis());
+    }
+    else {
+      lap.add(System.currentTimeMillis() - this.lap.get(lap.size()-1));
+    }
+  }
+  
+  /*
+   * Synchronizing object of StopwatchDriver to change state
+   */
+  private synchronized void changeState(State state) {
+    this.state = state;
+  }
+  
+  /*
+   * Synchronizing object of StopwatchDriver to clear Lap
+   */
+  private synchronized void clearLap() {
+    lap.clear();
   }
   
   @Override
@@ -25,8 +50,8 @@ public class StopwatchDriver implements Stopwatch {
     if (this.state == State.RUNNING) {
       throw new IllegalStateException("Stopwatch is alredy in running state");
     }
-    this.state = State.RUNNING;
-    this.lap.add(System.currentTimeMillis());
+    changeState(State.RUNNING);
+    addLap();
   }
 
   @Override
@@ -34,23 +59,19 @@ public class StopwatchDriver implements Stopwatch {
     if (this.state == State.BLOCK) {
       throw new IllegalStateException("Stopwatch is not running");
     }
-    synchronized (lap) {
-      this.lap.add(System.currentTimeMillis() - this.lap.get(lap.size()-1));
-    }
+    addLap();
   }
 
   @Override
   public void stop() {
     lap();
-    this.state = State.BLOCK;
+    changeState(State.BLOCK);
   }
 
   @Override
   public void reset() {
-    this.state = State.BLOCK;
-    synchronized (lap) {
-      lap.clear();
-    }
+    changeState(State.BLOCK);
+    clearLap();
   }
 
   @Override
@@ -60,12 +81,38 @@ public class StopwatchDriver implements Stopwatch {
   
   @Override
   public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof StopwatchDriver)) {
+      return false;
+    }
+    StopwatchDriver stopwatch = (StopwatchDriver)o;
+    if (id != null && id.equals(stopwatch.getId()) || (id == null && stopwatch.getId() == null)) {
+      List<Long> newLap = stopwatch.getLapTimes();
+      if (lap.size() != newLap.size()) {
+        return false;
+      }
+      for (int i = 0; i < lap.size(); i++) {
+        if (lap.get(i) != newLap.get(i)) {
+          return false;
+        }
+      }
+      return true;
+    }
     return false;
   }
   
   @Override
   public int hashCode() {
-    return 0;
+    int result = 17;
+    result += result * 31 + id.hashCode();
+    result += result * 31 + state.hashCode();
+    for (Long lapTimes: lap) {
+      long f = Long.valueOf(lapTimes);
+      result += (int) (f ^ (f >>> 32));
+    }
+    return result;
   }
   
   @Override
