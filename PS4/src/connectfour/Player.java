fa6af5d187b2434acfe2841bcd3f45b1dec44857
ShahNirali <nns271@nyu.edu>
2016-12-06 18:28:05 -0500

    Added exception handling

--- PS4/src/connectfour/Player.java
+++ PS4/src/connectfour/Player.java
@@ -16,8 +16,12 @@ public class Player {
    * Constructor of Player.
    * @param name Name of the player
    * @param color Color of Disc when player plays his turn
+   * @throws IllegalArgumentException when name or color is empty
    */
   public Player(String name, Color color) {
+    if (name == null || color == null || name.trim().length() == 0) {
+      throw new IllegalArgumentException("Name and Color can't be empty");
+    }
     this.name = name;
     this.color = color;
   }
