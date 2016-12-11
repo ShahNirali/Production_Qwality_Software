76b51e52c2a652a0faa55d615bdd02c655fa4949
ShahNirali <nns271@nyu.edu>
2016-12-10 22:13:33 -0500

    Added changes to draw an image

--- PS5/src/canvas/Canvas.java
+++ PS5/src/canvas/Canvas.java
@@ -4,30 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Canvas {
-  private Shape image;
   private List<CanvasListner> listeners = new ArrayList<CanvasListner>();
   
   public Canvas() {
-    image = new Shape();
-  }
-  
-  public void draw() {
-    fireDrawingShowEvent(image);
-  }
-  private void fireDrawingShowEvent(Shape drawnImage) {
-    for (CanvasListner listner: listeners) {
-      listner.showDrawing(drawnImage);
-    }
-  }
-
-  public void startPainting() {
-    fireDrawingStartEvent();
-  }
-
-  private void fireDrawingStartEvent() {
-    for (CanvasListner listner: listeners) {
-      listner.startDrawing();
-    }
   }
   
   public void addCanvasListener(CanvasListner canvasListner) {
@@ -36,5 +15,24 @@ public class Canvas {
 
   public void removeCanvasListener(CanvasListner canvasListner) {
     this.listeners.remove(canvasListner);
+  }
+  
+  public void startPainting() {
+    fireDrawingStartEvent();
+  }
+  
+  public void draw(Shape image) {
+    fireDrawingShowEvent(image);
+  }
+  private void fireDrawingShowEvent(Shape drawnImage) {
+    for (CanvasListner listner: listeners) {
+      listner.showDrawing(drawnImage);
+    }
+  }
+
+  private void fireDrawingStartEvent() {
+    for (CanvasListner listner: listeners) {
+      listner.startDrawing();
+    }
   }
 }
--- PS5/src/canvas/Shape.java
+++ PS5/src/canvas/Shape.java
@@ -1,5 +1,50 @@
 package canvas;
 
 public class Shape {
+  private int x1;
+  private int y1;
+  private int x2;
+  private int y2;
   
+  public Shape() {
+  }
+  
+  public Shape(int x1, int y1, int x2, int y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+  
+  public void setX1(int x) {
+    x1 = x;
+  }
+  
+  public void setY1(int y) {
+    y1 = y;
+  }
+  
+  public void setX2(int x) {
+    x2 = x;
+  }
+  
+  public void setY2(int y) {
+    y2 = y;
+  }
+  
+  public int getX1() {
+    return x1;
+  }
+  
+  public int getY1() {
+    return y1;
+  }
+  
+  public int getX2() {
+    return x2;
+  }
+  
+  public int getY2() {
+    return y2;
+  }
 }
