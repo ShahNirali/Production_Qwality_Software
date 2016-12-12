09a87617fe0fc26b457d97bd466ad0bc7cd81bb0
ShahNirali <nns271@nyu.edu>
2016-12-12 03:23:22 -0500

    Adding Test cases

--- PS5/testsrc/canvas/CanvasTest.java
+++ PS5/testsrc/canvas/CanvasTest.java
@@ -1,5 +1,105 @@
 package canvas;
 
-public class CanvasTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+public class CanvasTest {
+  
+  @Test
+  public void testAddListner_simple() {
+    Canvas canvas = new Canvas();
+    canvas.addCanvasListener(new CanvasListner() {
+      
+      @Override
+      public void startDrawing() {
+        showMessage("Canvas Started");
+      }
+      
+      private void showMessage(String message) {
+        assertTrue(message.equals("Canvas Started"));
+      }
+
+      @Override
+      public void showDrawing(Shape image) {
+      }
+    });
+    canvas.startPainting();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testAddListner_withNullListner() {
+    Canvas canvas = new Canvas();
+    canvas.addCanvasListener(null);
+  }
+  
+  @Test
+  public void testRemoveListner_simple() {
+    Canvas canvas = new Canvas();
+    CanvasListner canvasListner = new CanvasListner() {
+      
+      @Override
+      public void startDrawing() {
+      }
+      
+      @Override
+      public void showDrawing(Shape image) {
+      }
+    };
+    canvas.addCanvasListener(canvasListner);
+    canvas.removeCanvasListener(canvasListner);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testRemoveListner_withNullListner() {
+    Canvas canvas = new Canvas();
+    canvas.removeCanvasListener(null);
+  }
+  
+  @Test
+  public void testStartPainting() {
+    Canvas canvas = new Canvas();
+    canvas.addCanvasListener(new CanvasListner() {
+      
+      @Override
+      public void startDrawing() {
+        showMessage("Canvas Started");
+      }
+      
+      private void showMessage(String message) {
+        assertTrue(message.equals("Canvas Started"));
+      }
+
+      @Override
+      public void showDrawing(Shape image) {
+      }
+    });
+    canvas.startPainting();
+  }
+  
+  @Test
+  public void testDraw_simple() {
+    Canvas canvas = new Canvas();
+    canvas.addCanvasListener(new CanvasListner() {
+      
+      @Override
+      public void startDrawing() {
+      }
+
+      @Override
+      public void showDrawing(Shape image) {
+        assertEquals("[2, 3, 4, 5]", image.toString());
+      }
+    });
+    canvas.startPainting();
+    canvas.draw(new Shape(2, 3, 4, 5));
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testDraw_withNullImage() {
+    Canvas canvas = new Canvas();
+    canvas.startPainting();
+    canvas.draw(null);
+  }
 }
