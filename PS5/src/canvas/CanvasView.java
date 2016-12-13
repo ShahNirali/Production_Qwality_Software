f4471e0b9fed0539baf9659e2051dded604f3e6f
ShahNirali <nns271@nyu.edu>
2016-12-13 02:44:12 -0500

    Removed unused class imports

--- PS5/src/canvas/CanvasView.java
+++ PS5/src/canvas/CanvasView.java
@@ -1,7 +1,6 @@
 package canvas;
 
 import java.awt.BorderLayout;
-import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
